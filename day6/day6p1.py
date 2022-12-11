da27f836d920aa0fabeef66ddfdd55e1f1265f1a
KevOdo <kev.odas@gmail.com>
2022-12-11 10:10:46 +0100

    day 6 part 1

---
+++ day6/day6p1.py
@@ -0,0 +1,28 @@
+f = open("input.txt", "r")
+line = f.read()
+
+marker = []
+for i in range(len(line)):
+    if(i > 2):
+        duplicate = False
+        for char in marker:
+            t = 0
+            for a in marker:
+                if a == char:
+                    t+= 1
+            if (t >= 2):
+                duplicate = True
+        if(not duplicate):
+            if(line[i] in marker):
+                marker.pop(0)
+                marker.append(line[i])
+            else:
+                marker.append(line[i])
+                print(i + 1)
+                print(marker)
+                break
+        else:
+            marker.pop(0)
+            marker.append(line[i])
+    else:
+        marker.append(line[i])
